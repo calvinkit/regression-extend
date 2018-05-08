f91af3fc1d58f382d785fc381b482bbc16be7fcd
Calvin Lai <calvin.lai@scotiabank.com>
2018-05-08 12:28:25 -0400

    added testing files

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-# regression-extend
-Extension of the current regression package to include few more statistics.
-Also allow array of x and y as inputs instead of points format
+## Installation
+This module works on node and in the browser. It is available as the 'regression-extend' package on [npm](https://www.npmjs.com/package/regression-extend).
 
+### npm
+
+```
+npm install --save regression-extend
+```
+
+## Usage
+
+```javascript
+var regression = require('regression-extend');
+var result = regression.linear([[0, 1], [32, 67], [12, 79]]);
+var gradient = result.equation[0];
+var result = regression.linear([0,32,12], [1,67,79]);
+var gradient = result.equation[0];
+var sse = result.sse;
+```
+
+Data can be passed into the model as an points array, or as xArray, yArray. The last (2nd or 3rd, depends on what's the input format) parameter can be used to configure the model. 
+For details, please consult regression package documentation.
+
+
+### Properties
+In addition to original regression properties, it includes the following: 
+
+- `residual`: an array of the y-y', which is the residual/prediction error
+- `me`: mean error of the residual 
+- `sse`: Sum of square error/residual
+- `mse`: Mean square error (MSE)
+- `smse': Square root of MSE 
+- 'se': Standard error of beta/equation[0] 
+- 'tstat': t-Statistics of beta
+- 'zscore': an array of zscore of the predictions
---
+++ test/test.js
@@ -0,0 +1,39 @@
+var util = require('util');
+var assert = require('assert');
+var regression = require('./../lib/regression');
+var stat = require('./../lib/statistics');
+
+describe('Regression', function() {
+    var x = [1,2,3,4,5,6,7,8,9,10];
+    var y = [3,5,7,9,11.5,12.99,15.1,17.2,19.03,20.98];
+    var result = regression.linear(x,y, {precision:5});
+    it('beta w/ array', function() {
+        assert.strictEqual(result.equation[0], 2.00497);;
+    });
+    it('alpha w/ array', function() {
+        assert.strictEqual(result.equation[1], 1.05267);;
+    });
+    it('mean error w/ array', function() {
+        assert.strictEqual(+result.me.toFixed(5), -0.00001);
+    });
+    it('SSE error w/ array', function() {
+        assert.strictEqual(+result.sse.toFixed(5), 0.23536);
+    });
+    it('SMSE error w/ array', function() {
+        assert.strictEqual(+result.smse.toFixed(5), 0.15342);
+    });
+    it('tStat error w/ array', function() {
+        assert.strictEqual(+result.tstat.toFixed(5), 106.17231);
+    });
+
+    var x = [1,2,3,4,5,6,7,8,9,10];
+    var y = [3,5,7,9,11.5,12.99,15.1,17.2,19.03,20.98];
+    var pts = x.map((e,i) => [e, y[i]]);
+    var result = regression.linear(pts, {precision:5});
+    it('beta w/ pts', function() {
+        assert.strictEqual(result.equation[0], 2.00497);;
+    });
+    it('alpha w/ pts', function() {
+        assert.strictEqual(result.equation[1], 1.05267);;
+    });
+});
